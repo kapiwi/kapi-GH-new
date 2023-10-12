7a0e1a510ea0f5aa2ed583193fe0527212cd3cc2
paulrobmsft <147707328+paulrobmsft@users.noreply.github.com>
2023-10-12 15:08:11 +0100

    Update creds.py

--- creds.py
+++ creds.py
@@ -2,3 +2,7 @@ hostname=thisdoesnotexist.mysql.database.azure.com
 username=mysql_admin_duuh
 password=54df54sd5f4sdf5s4df
 ssl-mode=require
+
+
+//key
+39Qfy6ddzIS1vdjKIze6W6yl1TtgFZbyiYB/5YC8XZ1D0UuRtJEkz03pOxKtGSv2ZrsA+mwAk0HC+AStZi7JwQ==
